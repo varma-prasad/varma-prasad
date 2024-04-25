2c93d2246716f27d82b19d8ae18af346bc6d6cf3
Varma Prasad <108605375+varma-prasad@users.noreply.github.com>
2024-04-25 22:30:08 +0530

    Website

---
+++ files/script.js
@@ -0,0 +1,35 @@
+
+var typed = new Typed(".text", {
+    strings: ["Data Analytics" , "Data Engineering" , "Database Development", "Quality Assurance"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.reveal');
+    function checkScroll() {
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        if (rect.top < windowHeight * 0.75) {
+            card.style.opacity = 1;
+            card.style.transform = 'translateY(0)';
+        }
+    });
+    }
+    checkScroll();
+    window.addEventListener('scroll', checkScroll);
+});
